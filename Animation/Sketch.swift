d8bff81016c14f3e72ec7f8428d2b478c2bd3154
Yuen, Geoff <gyuen@lcs-mba3098.lcs.on.ca>
2017-11-03 22:25:18 -0400

    Finished Challenge 1

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var q : Int
     
     // This function runs once
     override init() {
@@ -17,7 +18,7 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
-        
+        q = 250
     }
     
     // Runs in a loop, forever, to create the animated effect
@@ -25,10 +26,31 @@ class Sketch : NSObject {
         
         // Change position
         x += 1
+        q -= 1
         
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.purple
+        canvas.borderColor = Color.purple
+        canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
+        
+        canvas.fillColor = Color.orange
+        canvas.borderColor = Color.orange
+        canvas.drawEllipse(centreX: q, centreY: 350, width: 50, height: 50)
+        
+        canvas.fillColor = Color.green
+        canvas.borderColor = Color.green
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        
+        canvas.fillColor = Color.blue
+        canvas.borderColor = Color.blue
+        canvas.drawEllipse(centreX: q, centreY: 150, width: 50, height: 50)
+        
+        canvas.fillColor = Color.black
+        canvas.borderColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
+        
         
     }
     
 }
+
