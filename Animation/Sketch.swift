cff701adecba85f9288ddaada58658dc851b22b7
lcs-gyuen <32303735+lcs-gyuen@users.noreply.github.com>
2017-11-08 23:07:43 -0500

    Update Sketch.swift
    I didn't use loops on this one

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -7,11 +7,19 @@ class Sketch : NSObject {
     let canvas : Canvas
     
     // Position of circle
-    var x : Double
-    var y : Double
-    var h : Double
+    var x : Int
+    var y : Int
+    var x2 : Int
+    var y2 : Int
+    var x3 : Int
+    var y3 : Int
+    var x4 : Int
+    var y4 : Int
+    //change the position
     var a : Double
-    var k : Double
+    var a2 : Double
+    var a3 : Double
+    var a4 : Double
     // This function runs once
     override init() {
         
@@ -20,21 +28,42 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 0
+        x2 = 0
+        x3 = 0
         y = 0
-        a = 0.008
-        h = -250
-        k = 500
+        y2 = 0
+        y3 = 0
+        x4 = 0
+        y4 = 0
+        a = -100/(50 * 50)
+        a2 = -150/(75 * 75)
+        a3 = -200/(100 * 100)
+        a4 = -230/(115 * 115)
+        
+        
+        
     }
+    
+    // Runs in a loop, forever, to create the animated effect
+    func draw() {
         
         // Change position
-        // Draw an ellipse in the middle of the canvas
-        
         x += 1
-        y = -(a*pow(x+h,2))+k
+        x2 += 1
+        x3 += 1
+        x4 += 1
+        y = Int(a * Double(x) * (Double(x)-100))
+        y2 = Int(a * Double(x2) * (Double(x2)-150))
+        y3 = Int(a * Double(x3) * (Double(x3)-200))
+        y4 = Int(a * Double(x4) * (Double(x4)-230))
         
-        // parabola function: y=a(x-h)^2+k
         
-        canvas.drawEllipse(centreX: Int(x), centreY: Int(y), width: 10, height: 10)
+        
+        // Draw an ellipse in the middle of the canvas
+        canvas.drawEllipse(centreX: x, centreY: y, width: 20, height: 20)
+        canvas.drawEllipse(centreX: x2, centreY: y2, width: 20, height: 20)
+        canvas.drawEllipse(centreX: x3, centreY: y3, width: 20, height: 20)
+        canvas.drawEllipse(centreX: x4, centreY: y4, width: 20, height: 20)
         
     }
     
